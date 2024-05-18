e14cf3199eadbf641f5fd0fd1627fe16edb6bf79
oyeyemi toheeb <opeyemioyeyemi141@gmail.com>
2024-05-18 14:47:56 +0100

    added navigatio and functionality of open and close when the nav icons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-"use strict";
-
-const currentYear = document.getElementById("date");
+'use strict';
+//Date setup
+const currentYear = document.getElementById('date');
 currentYear.innerHTML = new Date().getFullYear();
+
+//Nabar
+const navigationBtn = document.querySelector('.nav-btn');
+const navigationClose = document.querySelector('.nav-close');
+const navbar = document.querySelector('.navbar');
+
+navigationBtn.addEventListener('click', function () {
+  navbar.classList.add('show');
+});
+
+navigationClose.addEventListener('click', function () {
+  navbar.classList.remove('show');
+});
